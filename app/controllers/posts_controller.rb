1582c1796abd1b14cd167e5549533ce29b199003
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-06-19 19:27:59 +0500

    Blog app - Views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
-    def index
-      @posts = Post.all
-    end
-  
-    def show
-      @posts = Post.find(params[:id])
-    end
+  def index
+    @posts = Post.all
+    @user = User.find(params[:user_id])
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+end
